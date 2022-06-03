5ce5118a851671d4e9cceb1c13704b61ecb0de2f
Igor Matsuoka <igor.mtka@gmail.com>
2022-06-03 18:27:01 -0300

    make it better

--- src/Data/BaseDatabase.ts
+++ src/Data/BaseDatabase.ts
@@ -9,7 +9,7 @@ export default abstract class BaseDatabase {
     client: "mysql",
     connection: {
       host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT || "3306"),
+      port: Number(process.env.DB_PORT || "3000"),
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
